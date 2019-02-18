2f8a48e192dd34e09c405c37d481709107ee6c4c
Alexander <alexl@netcloud.co.il>
2019-02-18 08:43:04 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel  from './components/search-panel';
-import  TodoList from './components/todo-list'
+import App from './components/app';
 
-const App = () => {
-
-    const  todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make Awesome App', important: true, id:2},
-        {label: 'Have a lunch', important: false, id:3}
-    ];
-
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    );
-};
-
-//const el = React.createElement('h1', null, 'Hello World!');
 
 
 ReactDom.render(<App/>, document.getElementById('root'));
